96582f45f350acaba6df239cb2352e7f5ff10cd8
abdalmajed <abdalmajed.h@clic.agency>
2018-09-28 16:34:15 +0300

    Initial Commit

---
+++ LiveBroadcaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveBroadcaster
+//
+//  Created by Abd on 9/28/18.
+//  Copyright © 2018 Abdulmajeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
